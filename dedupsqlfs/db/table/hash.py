ecdcf73a75cf91a9c3642d801161adef00d0b49b
Sergey Dryabzhinsky <sergey@rusoft.ru>
2013-09-14 13:14:06 +0400

    updates:
    Move compression_type field from hash to index table
    Reduce memory usage in gc_hook

--- dedupsqlfs/db/table/hash.py
+++ dedupsqlfs/db/table/hash.py
@@ -16,35 +16,29 @@ class TableHash( Table ):
         c.execute(
             "CREATE TABLE IF NOT EXISTS `%s` (" % self._table_name+
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                "compression_type_id INTEGER NOT NULL, "+
                 "hash BLOB NOT NULL UNIQUE"+
             ")"
         )
-        c.execute(
-            "CREATE INDEX IF NOT EXISTS b_compression_type ON `%s` (" % self._table_name+
-                "compression_type_id"+
-            ");"
-        )
         return
 
-    def insert( self, compression_type_id, value):
+    def insert( self, value):
         cur = self.getCursor()
         bvalue = sqlite3.Binary(value)
-        cur.execute("INSERT INTO `%s`(compression_type_id, hash) VALUES (?,?)" % self._table_name,
-                    (compression_type_id, bvalue,))
+        cur.execute("INSERT INTO `%s`(hash) VALUES (?)" % self._table_name,
+                    (bvalue,))
         item = cur.lastrowid
         self.commit()
         return item
 
-    def update( self, item_id, compression_type_id, value ):
+    def update( self, item_id, value ):
         """
         @return: count updated rows
         @rtype: int
         """
         cur = self.getCursor()
         bvalue = sqlite3.Binary(value)
-        cur.execute("UPDATE `%s` SET compression_type_id=?, hash=? WHERE id=?" % self._table_name,
-                    (compression_type_id, bvalue, item_id))
+        cur.execute("UPDATE `%s` SET hash=? WHERE id=?" % self._table_name,
+                    (bvalue, item_id))
         count = cur.rowcount
         self.commit()
         return count
@@ -71,11 +65,5 @@ class TableHash( Table ):
         if item:
             item = item["id"]
         return item
-
-    def count_compression_type( self ):
-        cur = self.getCursor()
-        cur.execute("SELECT COUNT(compression_type_id) AS cnt,compression_type_id FROM `%s` GROUP BY compression_type_id" % self._table_name)
-        items = cur.fetchall()
-        return items
 
     pass

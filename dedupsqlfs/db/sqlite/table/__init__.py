013394f5c7ff6c78874a423b647cdc8cd4ae98f1
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2014-02-16 07:27:11 +0400

    Update str formating values

--- dedupsqlfs/db/sqlite/table/__init__.py
+++ dedupsqlfs/db/sqlite/table/__init__.py
@@ -264,7 +264,7 @@ class Table( object ):
 
         newSize = os.path.getsize(fn)
 
-        self.getLogger().info("DB size change after vacuum: %.2f%%" % ((oldSize - newSize) * 100.0 / oldSize,))
+        self.getLogger().info("DB '%s' size change after vacuum: %.2f%%" % (self.getName(), (oldSize - newSize) * 100.0 / oldSize,))
 
         self.stopTimer("vacuum")
         return self

ee008cae9fed1ec326bd3b6042bdc0dadb08711a
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2015-02-12 00:29:39 +0300

    Fix get ID

--- dedupsqlfs/db/mysql/table/tmp_ids.py
+++ dedupsqlfs/db/mysql/table/tmp_ids.py
@@ -66,7 +66,7 @@ class TableTmpIds( Table ):
             cur = self.getCursor()
             cur.execute("SELECT `id` FROM `%s` " % self.getName()+
                         " WHERE `id` IN (%s)" % (id_str,))
-            ret_ids = tuple(str(item["inode_id"]) for item in cur)
+            ret_ids = tuple(str(item["id"]) for item in cur)
         self.stopTimer('get_ids_by_ids')
         return ret_ids
 

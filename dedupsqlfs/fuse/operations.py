fdc87b984cdb24d5e16406aea87ceac27e5c0b7e
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2017-05-13 00:57:57 +0300

    Fix block flushing

--- dedupsqlfs/fuse/operations.py
+++ dedupsqlfs/fuse/operations.py
@@ -2169,8 +2169,8 @@ class DedupOperations(llfuse.Operations): # {{{1
 
         for inode, inode_data in cached_blocks.items():
             for block_number, block_data in inode_data.items():
-                if block_data["w"]:
-                    block = block_data.get("block")
+                if block_data[self.cached_blocks.OFFSET_WRITTEN]:
+                    block = block_data[self.cached_blocks.OFFSET_BLOCK]
                     item = self.__write_block_data(int(inode), int(block_number), block)
                     if item["hash"] and (item["new"] or item["recompress"]):
                         blocksToCompress[ item["hash"] ] = item["data"]

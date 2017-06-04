d62cc4d4e401c6bd77bcb71f5c1d266594d6f92d
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2017-06-04 03:16:51 +0300

    Fix module name, update simple tests

--- lib-dynload/lzo/setup.py
+++ lib-dynload/lzo/setup.py
@@ -23,13 +23,13 @@ else:
         extra_compile_args.insert(0, "-O2")
 
 setup(
-    name = "lzo2",
+    name = "lzo",
     version = "1.0",
     packages=[],
     package_dir={'': 'src'},
     ext_modules = [
         Extension(
-            "_lzo2",
+            "_lzo",
             ["src/lzomodule.c"],
             libraries=['lzo2'],
             extra_compile_args=extra_compile_args

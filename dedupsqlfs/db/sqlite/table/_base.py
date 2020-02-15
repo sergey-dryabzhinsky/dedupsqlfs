0335d7f31399183c4643f74f423747b48d376c3d
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2020-02-15 03:11:41 +0300

    For #184
    - disable timers by default
    - hide error of umount command

--- dedupsqlfs/db/sqlite/table/_base.py
+++ dedupsqlfs/db/sqlite/table/_base.py
@@ -38,6 +38,7 @@ class Table( object ):
 
     _compressed_prog = None
 
+    _enable_timers = False
 
     def __init__(self, manager):
         if self._table_name is None:
@@ -56,16 +57,24 @@ class Table( object ):
             self._log.addHandler(logging.StreamHandler(sys.stderr))
         return self._log
 
+    def setEnableTimers(self, flag=True):
+        self._enable_timers = flag is True
+        return self
+
     def getOperationsCount(self):
         return self._op_count
 
     def getAllOperationsCount(self):
         s = 0
+        if not self._enable_timers:
+            return s
         for op, c in self._op_count.items():
             s += c
         return s
 
     def incOperationsCount(self, op):
+        if not self._enable_timers:
+            return self
         if not (op in self._op_count):
             self._op_count[ op ] = 0
         self._op_count[ op ] += 1
@@ -76,24 +85,29 @@ class Table( object ):
 
     def getAllTimeSpent(self):
         s = 0
+        if not self._enable_timers:
+            return s
         for op, t in self._time_spent.items():
             s += t
         return s
 
     def incOperationsTimeSpent(self, op, start_time):
+        if not self._enable_timers:
+            return self
         if not (op in self._time_spent):
             self._time_spent[ op ] = 0
         self._time_spent[ op ] += time() - start_time
         return self
 
     def startTimer(self):
+        if not self._enable_timers:
+            return self
         self._last_time = time()
         return self
 
     def stopTimer(self, op):
-        # Too slow =(
-        # caller = inspect.currentframe().f_back
-        # op = inspect.getframeinfo(caller)[2]
+        if not self._enable_timers:
+            return self
 
         self.incOperationsCount(op)
         self.incOperationsTimeSpent(op, self._last_time)

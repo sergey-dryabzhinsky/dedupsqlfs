1e71dd73a083bf14d35c17ee42d40f2b208237ba
Sergey Dryabzhinsky <sergey.dryabzhinsky@gmail.com>
2021-05-19 08:37:35 +0300

    Same as recompress:
    - do less methods access
    - do less output formating
    - commit every 5000-th hash

--- dedupsqlfs/app/actions/rehash.py
+++ dedupsqlfs/app/actions/rehash.py
@@ -18,11 +18,13 @@ def do_rehash(options, _fuse):
     @type  _fuse: dedupsqlfs.fuse.dedupfs.DedupFS
     """
 
+    isVerbosity = _fuse.getOption("verbosity") > 0
+
     tableOption = _fuse.operations.getTable("option")
 
     curHashFunc = tableOption.get("hash_function")
     if curHashFunc == options.rehash_function:
-        if _fuse.getOption("verbosity") > 0:
+        if isVerbosity:
             print("Already using %s hash function for filesystem! Do not rehashing." % curHashFunc)
         return True
 
@@ -33,14 +35,20 @@ def do_rehash(options, _fuse):
     _fuse.operations.hash_function = options.rehash_function
 
     hashCount = tableHash.get_count()
-    if _fuse.getOption("verbosity") > 0:
+    if isVerbosity:
         print("Ready to rehash %s blocks." % hashCount)
 
     cur = tableHash.getCursor(True)
     cur.execute("SELECT `id` FROM `%s`" % tableHash.getName())
 
-    cnt = upd = 0
-    lastPrc = ""
+    # Every 100*100 (4x symbols)
+    cntNth = int(hashCount/10000.0)
+    if cntNth < 1:
+        cntNth = 1
+
+    # Process Nth blocks and then - commit
+    maxCmmt = 5000
+    cnt = cntNext = upd = cmmt = 0
 
     _fuse.operations.getManager().setAutocommit(False)
     tableHash.begin()
@@ -49,6 +57,7 @@ def do_rehash(options, _fuse):
     for hashItem in iter(cur.fetchone, None):
 
         cnt += 1
+        cmmt += 1
 
         blockItem = tableBlock.get(hashItem["id"])
         hashCT = tableHashCT.get(hashItem["id"])
@@ -58,18 +67,25 @@ def do_rehash(options, _fuse):
 
         if res:
             upd += 1
-        prc = "%6.2f%%" % (cnt*100.0/hashCount)
-        if prc != lastPrc:
-            lastPrc = prc
-            if _fuse.getOption("verbosity") > 0:
+
+        if cnt >= cntNext:
+            cntNext += cntNth
+            if isVerbosity:
+                prc = "%6.2f%%" % (cnt * 100.0 / hashCount)
                 sys.stdout.write("\r%s " % prc)
                 sys.stdout.flush()
 
-    if _fuse.getOption("verbosity") > 0:
+        if cmmt >= maxCmmt:
+            _fuse.operations.getManager().setAutocommit(False)
+            tableHash.commit()
+            tableHash.begin()
+            _fuse.operations.getManager().setAutocommit(True)
+
+    if isVerbosity:
         sys.stdout.write("\n")
         sys.stdout.flush()
 
-    if _fuse.getOption("verbosity") > 0:
+    if isVerbosity:
         print("Processed %s hashes, rehashed %s blocks." % (cnt, upd,))
 
     if hashCount == cnt:

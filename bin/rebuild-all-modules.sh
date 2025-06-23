e804d772f5a1e01c13a0a6b9bc6e3e599b128679
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-06-23 18:40:46 +0300

    Update script foor rebuilding modules

--- bin/rebuild-all-modules.sh
+++ bin/rebuild-all-modules.sh
@@ -17,9 +17,10 @@ PY=`which $PY`
 
 EXTRA_OPT=$1
 if [ -n "${EXTRA_OPT}" ]; then
-#	EXTRA_OPT="--extra-optimization"
+	EXTRA_OPT="--extra-optimization"
 	export RC_EXTRAOPT=1
 	export CFLAGS="-O3 -march=native"
+	export CXXFLAGS="-O3 -march=native"
 else
 	EXTRA_OPT=""
 fi

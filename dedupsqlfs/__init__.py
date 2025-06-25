d268eb33206f41b5d20f3f146d8de2b58d5c6ef9
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-06-25 11:38:05 +0300

    Pre 1.2.955

--- dedupsqlfs/__init__.py
+++ dedupsqlfs/__init__.py
@@ -26,7 +26,7 @@ __name__ = "DedupSQLfs"
 __fsname__ = "dedupsqlfs"
 __fsversion__ = "3.8"
 # Future 1.3
-__version__ = "1.2.955-dev"
+__version__ = "1.2.955"
 
 # Check the Python version, warn the user if untested.
 import sys

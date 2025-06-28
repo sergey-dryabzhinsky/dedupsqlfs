d8e544f6f5e97e6b09fa3b33733bb2bd849b16b4
Sergey Dryabzhinsky <sergey.dryabzhinsky+github@gmail.com>
2025-06-28 18:31:43 +0300

    First version helper class with timers for operation calls for debug

---
+++ dedupsqlfs/lib/timers_ops.py
@@ -0,0 +1,79 @@
+# -*- coding: utf8 -*-
+
+__author__ = 'sergey'
+
+from time import time
+
+class TimersOps( object ):
+
+    _last_time = None
+    _time_spent = None
+    _op_count = None
+
+    _log = None
+
+    _enable_timers = False
+
+    def __init__(self, manager):
+        self._time_spent = {}
+        self._op_count = {}
+        pass
+
+    def setEnableTimers(self, flag=True):
+        self._enable_timers = flag is True
+        return self
+
+    def getOperationsCount(self):
+        return self._op_count
+
+    def getAllOperationsCount(self):
+        s = 0
+        if not self._enable_timers:
+            return s
+        for op, c in self._op_count.items():
+            s += c
+        return s
+
+    def incOperationsCount(self, op):
+        if not self._enable_timers:
+            return self
+        if not (op in self._op_count):
+            self._op_count[ op ] = 0
+        self._op_count[ op ] += 1
+        return self
+
+    def getTimeSpent(self):
+        return self._time_spent
+
+    def getAllTimeSpent(self):
+        s = 0
+        if not self._enable_timers:
+            return s
+        for op, t in self._time_spent.items():
+            s += t
+        return s
+
+    def incOperationsTimeSpent(self, op, start_time):
+        if not self._enable_timers:
+            return self
+        if not (op in self._time_spent):
+            self._time_spent[ op ] = 0
+        self._time_spent[ op ] += time() - start_time
+        return self
+
+    def startTimer(self):
+        if not self._enable_timers:
+            return self
+        self._last_time = time()
+        return self
+
+    def stopTimer(self, op):
+        if not self._enable_timers:
+            return self
+
+        self.incOperationsCount(op)
+        self.incOperationsTimeSpent(op, self._last_time)
+
+        self._last_time = None
+        return self
+
